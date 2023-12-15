b2385aff9351a73e3148b23ba16d4be9a64df8bd
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-15 16:55:51 +0100

    Added clear functionnality

--- script.js
+++ script.js
@@ -15,7 +15,18 @@ function setNum() {
   viewer.innerHTML = currentNum; // Display current number
 }
 
+function clear() {
+  currentNum = "";
+  previousNum = "";
+  resultNum = "";
+  operator = "";
+  viewer.innerHTML = "0";
+}
+
 // Add click event to numbers
 for (var i = 0, l = nums.length; i < l; i++) {
   nums[i].onclick = setNum;
 }
+
+// Add click event to clear
+document.querySelector("#clear").onclick = clear;
