de3b306505c50928f2199da899e9b45bf8300306
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-20 12:23:23 +0100

    add clearEntry button click

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const viewer_accumulator = document.querySelector("#accumulator"), // Viewer for previous number
   viewer_current = document.querySelector("#current"), // Viewer for current number
   calc_clear = document.getElementById("clear"), // Clear button
+  calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
   calc_nums = document.querySelectorAll(".num"), // List of numbers
   calc_ops = document.querySelectorAll(".ops"), // List of operators
   calc_equals = document.getElementById("equals"); // Equal button
-// calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
 // calc_backspace = document.getElementById("backspace"), // Backspace button
 // calc_inverse = document.getElementById("inverse"), // Inverse button
 // calc_square = document.getElementById("square"), // Square button
@@ -16,6 +16,11 @@ let calculator = new Calculator();
 
 calc_clear.addEventListener("click", () => {
   calculator.clear();
+  calculator.updateViewer();
+});
+
+calc_clear_entry.addEventListener("click", () => {
+  calculator.clearEntry();
   calculator.updateViewer();
 });
 
