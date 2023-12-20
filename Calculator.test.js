251865baacca214c6f7191b3650c9427706faae1
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-20 10:41:41 +0100

    renammed method in tests

--- Calculator.test.js
+++ Calculator.test.js
@@ -6,7 +6,7 @@ beforeEach(() => {
   calculator = new Calculator();
 });
 
-test("addNumber should append a number to currentNum string", () => {
+test("appendNum should append a number to currentNum string", () => {
   // Given
   calculator.currentNum = "66";
 
@@ -17,7 +17,7 @@ test("addNumber should append a number to currentNum string", () => {
   expect(calculator.currentNum).toBe("666");
 });
 
-test("addNumber should replace 0 with the number selected", () => {
+test("appendNum should replace 0 with the number selected", () => {
   // Given
   calculator.currentNum = "0";
 
