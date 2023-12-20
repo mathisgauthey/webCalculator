fd55eaef34527952afa432aa44326ffc2778d762
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-20 11:51:38 +0100

    add equals button action

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const viewer_accumulator = document.querySelector("#accumulator"), // Viewer for
   viewer_current = document.querySelector("#current"), // Viewer for current number
   calc_clear = document.getElementById("clear"), // Clear button
   calc_nums = document.querySelectorAll(".num"), // List of numbers
-  calc_ops = document.querySelectorAll(".ops"); // List of operators
-// calc_equals = document.getElementById("equals"), // Equal button
+  calc_ops = document.querySelectorAll(".ops"), // List of operators
+  calc_equals = document.getElementById("equals"); // Equal button
 // calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
 // calc_backspace = document.getElementById("backspace"), // Backspace button
 // calc_inverse = document.getElementById("inverse"), // Inverse button
@@ -31,4 +31,9 @@ calc_ops.forEach((button) => {
     calculator.operatorSelect(button.innerHTML);
     calculator.updateViewer();
   });
+});
+
+calc_equals.addEventListener("click", () => {
+  calculator.equals();
+  calculator.updateViewer();
 });
