59aa81461764e0863789a4c6bb0b4819369c255c
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-20 09:57:07 +0100

    Add clear and refactor clear and appendNum

--- script.js
+++ script.js
@@ -17,31 +17,37 @@ class Calculator {
     this.currentNum += num;
   }
 
-  updateViewer(x, y) {
-    x.innerHTML = this.accumulatorNum;
-    y.innerHTML = this.currentNum;
+  updateViewer() {
+    viewer_accumulator.innerHTML = this.accumulatorNum;
+    viewer_current.innerHTML = this.currentNum;
   }
 }
 
 const viewer_accumulator = document.querySelector("#accumulator"), // Viewer for previous number
   viewer_current = document.querySelector("#current"), // Viewer for current number
-  calc_nums = document.querySelectorAll(".num"), // List of numbers
-  calc_ops = document.querySelectorAll(".ops"), // List of operators
-  calc_equals = document.getElementById("equals"), // Equal button
   calc_clear = document.getElementById("clear"), // Clear button
-  calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
-  calc_backspace = document.getElementById("backspace"), // Backspace button
-  calc_inverse = document.getElementById("inverse"), // Inverse button
-  calc_square = document.getElementById("square"), // Square button
-  calc_sqrt = document.getElementById("sqrt"), // Square root button
-  calc_percentage = document.getElementById("percentage"), // Percentage button
-  calc_plus_minus = document.getElementById("plus_minus"); // Plus minus button
+  calc_nums = document.querySelectorAll(".num"); // List of numbers
+
+// calc_ops = document.querySelectorAll(".ops"), // List of operators
+// calc_equals = document.getElementById("equals"), // Equal button
+// calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
+// calc_backspace = document.getElementById("backspace"), // Backspace button
+// calc_inverse = document.getElementById("inverse"), // Inverse button
+// calc_square = document.getElementById("square"), // Square button
+// calc_sqrt = document.getElementById("sqrt"), // Square root button
+// calc_percentage = document.getElementById("percentage"), // Percentage button
+// calc_plus_minus = document.getElementById("plus_minus"); // Plus minus button
 
 let calculator = new Calculator();
+
+calc_clear.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateViewer();
+});
 
 calc_nums.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNum(button.innerHTML);
-    calculator.updateViewer(viewer_accumulator, viewer_current);
+    calculator.updateViewer();
   });
 });
