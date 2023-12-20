9e33b043ce2f8b575c79528abe1ff67a725e0631
mathisgauthey <mathis.gauthey@gmail.com>
2023-12-20 11:23:24 +0100

    add operators button click

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const viewer_accumulator = document.querySelector("#accumulator"), // Viewer for previous number
   viewer_current = document.querySelector("#current"), // Viewer for current number
   calc_clear = document.getElementById("clear"), // Clear button
-  calc_nums = document.querySelectorAll(".num"); // List of numbers
-
-// calc_ops = document.querySelectorAll(".ops"), // List of operators
+  calc_nums = document.querySelectorAll(".num"), // List of numbers
+  calc_ops = document.querySelectorAll(".ops"); // List of operators
 // calc_equals = document.getElementById("equals"), // Equal button
 // calc_clear_entry = document.getElementById("clear_entry"), // Clear entry button
 // calc_backspace = document.getElementById("backspace"), // Backspace button
@@ -23,6 +22,13 @@ calc_clear.addEventListener("click", () => {
 calc_nums.forEach((button) => {
   button.addEventListener("click", () => {
     calculator.appendNum(button.innerHTML);
+    calculator.updateViewer();
+  });
+});
+
+calc_ops.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.operatorSelect(button.innerHTML);
     calculator.updateViewer();
   });
 });
